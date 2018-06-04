9bee6d9300487ed3593feadc074e52ca26dba0ed
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2018-06-04 22:31:42 +0200

    ZEN: synaptics: Add support for some old Dell clickpads
    Adjusted for v4.17.
    
    Squashed commit of the following:
    
    commit 8105b9d1a151676539876c9e29a8c23453edf1aa
    Author: Chase Douglas <chase.douglas@canonical.com>
    Date:   Fri Feb 24 15:05:51 2012 -0800
    
        UBUNTU: SAUCE: (no-up) Input: synapticss - Set buttonpad property for all clickpads
    
        no-up comments: Keep both, so long as they continue to apply cleanly.
        The patches apply only to a couple of old Dell minis, and Dell has said
        they don't intend to use those touchpads again. Upstreaming these
        patches stalled due to lack of information/response, and continuing to
        pursue it probably isn't worth the effort, so they can be marked no-up.
        There's no harm in keeping the patches, but if they become a problem
        they can be dropped.
    
        Signed-off-by: Chase Douglas <chase.douglas@canonical.com>
        Acked-by: Seth Forshee <seth.forshee@canonical.com>
        Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
        Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    
    commit 5999116a6ae07afa94332409b97d1a4ce8666735
    Author: Chase Douglas <chase.douglas@canonical.com>
    Date:   Fri Feb 24 15:05:50 2012 -0800
    
        UBUNTU: SAUCE: (no-up) Input: synaptics - add second variant of two-button clickpad
    
        This is necessary for clickpad detection of Synaptics trackpads in Dell
        Mini 10 series of laptops.
    
        no-up comments: Keep both, so long as they continue to apply cleanly.
        The patches apply only to a couple of old Dell minis, and Dell has said
        they don't intend to use those touchpads again. Upstreaming these
        patches stalled due to lack of information/response, and continuing to
        pursue it probably isn't worth the effort, so they can be marked no-up.
        There's no harm in keeping the patches, but if they become a problem
        they can be dropped.
    
        Signed-off-by: Chase Douglas <chase.douglas@canonical.com>
        Acked-by: Seth Forshee <seth.forshee@canonical.com>
        Acked-by: Andy Whitcroft <andy.whitcroft@canonical.com>
        Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/input/mouse/synaptics.c
+++ drivers/input/mouse/synaptics.c
@@ -1338,7 +1338,9 @@ static int set_input_params(struct psmouse *psmouse,
 		if (psmouse_matches_pnp_id(psmouse, topbuttonpad_pnp_ids) &&
 		    !SYN_CAP_EXT_BUTTONS_STICK(info->ext_cap_10))
 			__set_bit(INPUT_PROP_TOPBUTTONPAD, dev->propbit);
-	}
+	} else if (SYN_CAP_CLICKPAD2BTN(info->ext_cap_0c) ||
+		   SYN_CAP_CLICKPAD2BTN2(info->ext_cap_0c))
+		__set_bit(INPUT_PROP_BUTTONPAD, dev->propbit);
 
 	return 0;
 }
