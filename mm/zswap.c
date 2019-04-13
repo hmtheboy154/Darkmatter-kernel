10fb7ff8ab73879cd51b889d3878f1af43742701
Steven Barrett <steven@liquorix.net>
2019-04-13 15:08:10 -0500

    ZEN: Enable zswap by default

--- mm/zswap.c
+++ mm/zswap.c
@@ -81,7 +81,11 @@ static u64 zswap_duplicate_entry;
 #define ZSWAP_PARAM_UNSET ""
 
 /* Enable/disable zswap (disabled by default) */
+#ifdef CONFIG_ZEN_INTERACTIVE
+static bool zswap_enabled = true;
+#else
 static bool zswap_enabled;
+#endif
 static int zswap_enabled_param_set(const char *,
 				   const struct kernel_param *);
 static struct kernel_param_ops zswap_enabled_param_ops = {
