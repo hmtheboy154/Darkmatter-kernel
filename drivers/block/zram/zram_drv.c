3bf7a408c5f4e6bcd08cb2f2308500fdc32f257f
Steven Barrett <steven@liquorix.net>
2019-04-17 12:02:55 -0500

    ZEN: Prefer zstd > lz4 > lzo in zram

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -41,7 +41,13 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
+#if IS_ENABLED(CONFIG_ZEN_INTERACTIVE) && IS_ENABLED(CONFIG_CRYPTO_ZSTD)
+static const char *default_compressor = "zstd";
+#elif IS_ENABLED(CONFIG_ZEN_INTERACTIVE) && IS_ENABLED(CONFIG_CRYPTO_LZ4)
+static const char *default_compressor = "lz4";
+#else
 static const char *default_compressor = "lzo";
+#endif
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
