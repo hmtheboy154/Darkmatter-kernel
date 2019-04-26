78fb15ac04bff56dfeb0b6fe692fb6e0ccf4e56b
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2019-04-26 02:04:43 +0200

    ZEN: Use Kconfig for compressor selection
    This simplifies the #ifs and also makes sure the default compressor is
    always available. This is particularly important in the case of zswap,
    which is always built-in and gets activated early, when we only have an
    initrd that may not contain the modules needed.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -41,10 +41,8 @@ static DEFINE_IDR(zram_index_idr);
 static DEFINE_MUTEX(zram_index_mutex);
 
 static int zram_major;
-#if IS_ENABLED(CONFIG_ZEN_INTERACTIVE) && IS_ENABLED(CONFIG_CRYPTO_LZ4)
+#ifdef CONFIG_ZEN_INTERACTIVE
 static const char *default_compressor = "lz4";
-#elif IS_ENABLED(CONFIG_ZEN_INTERACTIVE) && IS_ENABLED(CONFIG_CRYPTO_ZSTD)
-static const char *default_compressor = "zstd";
 #else
 static const char *default_compressor = "lzo";
 #endif
