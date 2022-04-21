f111e636360a16eb5b33d031b5603d18876f7adc
hmtheboy154 <buingoc67@gmail.com>
2022-04-21 12:16:10 +0700

    Revert "ANDROID: GKI: set vfs-only exports into their own namespace"
    This reverts commit 176c157bbeb7a6d1ed2a2aec13321aa5b1f07da5.
    
     Conflicts:
    	fs/attr.c
    	fs/namei.c

--- fs/open.c
+++ fs/open.c
@@ -1170,7 +1170,7 @@ struct file *filp_open(const char *filename, int flags, umode_t mode)
 	}
 	return file;
 }
-EXPORT_SYMBOL_NS(filp_open, ANDROID_GKI_VFS_EXPORT_ONLY);
+EXPORT_SYMBOL(filp_open);
 
 /* ANDROID: Allow drivers to open only block files from kernel mode */
 struct file *filp_open_block(const char *filename, int flags, umode_t mode)
@@ -1402,7 +1402,7 @@ int generic_file_open(struct inode * inode, struct file * filp)
 	return 0;
 }
 
-EXPORT_SYMBOL_NS(generic_file_open, ANDROID_GKI_VFS_EXPORT_ONLY);
+EXPORT_SYMBOL(generic_file_open);
 
 /*
  * This is used by subsystems that don't want seekable
