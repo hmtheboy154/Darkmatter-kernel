702061b07fb71fa47a7c0b8669162ee58b404901
Vicki Pfau <vi@endrift.com>
2022-07-05 19:19:30 -0700

    Input: xpad - add support for HORIPAD FPS for Nintendo Switch
    Signed-off-by: Vicki Pfau <vi@endrift.com>
    (cherry picked from commit add53ac5e23510c040381200a2d6881ece284856)
    [Fwd ported to v6.1]
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/input/joystick/xpad.c
+++ drivers/input/joystick/xpad.c
@@ -2049,6 +2049,13 @@ static int xpad_probe(struct usb_interface *intf, const struct usb_device_id *id
 		goto err_free_in_urb;
 	}
 
+	if (xpad->xtype == XTYPE_XBOX360) {
+		/* Some third-party controllers Xbox 360-style controllers
+		 * require this message to finish initialization */
+		usb_control_msg(udev, usb_sndctrlpipe(udev, 0),
+				1, 0xc1, 0x100, 0, NULL, 20, 25);
+	}
+
 	ep_irq_in = ep_irq_out = NULL;
 
 	for (i = 0; i < 2; i++) {
