c3e1b4f84ee99ed1a29f29a5ddc024ec78865334
Sung Joon Kim <sungkim@amd.com>
2022-09-29 15:35:47 -0400

    fix unpowered dock sleep/wake no display issue in MST mode
    (cherry picked from commit 8fdf399070d9c3e391234f12754ebab8d66cd8ed)
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -2252,13 +2252,13 @@ static void s3_handle_mst(struct drm_device *dev, bool suspend)
 		} else {
 			ret = drm_dp_mst_topology_mgr_resume(mgr, true);
 			if (ret < 0) {
-				mutex_lock(&aconnector->hpd_lock);
 				aconnector->dc_link->mst_dpcd_fail_on_resume = true;
 				is_dp_alt_mode = wait_for_entering_dp_alt_mode(aconnector->dc_link);
 				if (is_dp_alt_mode) {
 					do {
 						ret = drm_dp_dpcd_read(mgr->aux, DP_DPCD_REV, mgr->dpcd, 1);
 					} while (ret != 1);
+					msleep(50);
 					ret = drm_dp_mst_topology_mgr_resume(mgr, true);
 				}
 
@@ -2267,7 +2267,6 @@ static void s3_handle_mst(struct drm_device *dev, bool suspend)
 						aconnector->dc_link);
 					need_hotplug = true;
 				}
-				mutex_unlock(&aconnector->hpd_lock);
 			}
 		}
 	}
