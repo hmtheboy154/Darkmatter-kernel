b9f272e0ddc8b825df49bea4f07b058505f68182
hmtheboy154 <buingoc67@gmail.com>
2022-11-07 02:43:15 +0700

    Replace ipts driver with new one and add iptsd driver
    Also update defconfig

--- drivers/hid/ipts/mei.c
+++ drivers/hid/ipts/mei.c
@@ -1,44 +1,50 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2016 Intel Corporation
- * Copyright (c) 2020 Dorian Stoll
+ * Copyright (c) 2020-2022 Dorian Stoll
  *
  * Linux driver for Intel Precise Touch & Stylus
  */
 
+#include <linux/completion.h>
 #include <linux/delay.h>
+#include <linux/device.h>
 #include <linux/dma-mapping.h>
 #include <linux/mei_cl_bus.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/slab.h>
+#include <linux/stddef.h>
+#include <linux/types.h>
 
 #include "context.h"
 #include "control.h"
-#include "protocol.h"
 #include "receiver.h"
-#include "uapi.h"
+#include "spec-device.h"
+
+/*
+ * The MEI client ID for IPTS functionality.
+ */
+#define IPTS_MEI_UUID                                                                              \
+	UUID_LE(0x3e8d0870, 0x271a, 0x4208, 0x8e, 0xb5, 0x9a, 0xcb, 0x94, 0x02, 0xae, 0x04)
 
 static int ipts_mei_set_dma_mask(struct mei_cl_device *cldev)
 {
-	int ret;
-
-	ret = dma_coerce_mask_and_coherent(&cldev->dev, DMA_BIT_MASK(64));
-	if (!ret)
+	if (!dma_coerce_mask_and_coherent(&cldev->dev, DMA_BIT_MASK(64)))
 		return 0;
 
 	return dma_coerce_mask_and_coherent(&cldev->dev, DMA_BIT_MASK(32));
 }
 
-static int ipts_mei_probe(struct mei_cl_device *cldev,
-			  const struct mei_cl_device_id *id)
+static int ipts_mei_probe(struct mei_cl_device *cldev, const struct mei_cl_device_id *id)
 {
 	int ret;
 	struct ipts_context *ipts;
 
-	if (ipts_mei_set_dma_mask(cldev)) {
-		dev_err(&cldev->dev, "Failed to set DMA mask for IPTS\n");
-		return -EFAULT;
+	ret = ipts_mei_set_dma_mask(cldev);
+	if (ret) {
+		dev_err(&cldev->dev, "Failed to set DMA mask for IPTS: %d\n", ret);
+		return ret;
 	}
 
 	ret = mei_cldev_enable(cldev);
@@ -47,7 +53,7 @@ static int ipts_mei_probe(struct mei_cl_device *cldev,
 		return ret;
 	}
 
-	ipts = kzalloc(sizeof(*ipts), GFP_KERNEL);
+	ipts = devm_kzalloc(&cldev->dev, sizeof(*ipts), GFP_KERNEL);
 	if (!ipts) {
 		mei_cldev_disable(cldev);
 		return -ENOMEM;
@@ -55,34 +61,33 @@ static int ipts_mei_probe(struct mei_cl_device *cldev,
 
 	ipts->cldev = cldev;
 	ipts->dev = &cldev->dev;
-	ipts->status = IPTS_HOST_STATUS_STOPPED;
+	ipts->mode = IPTS_MODE_EVENT;
 
 	mei_cldev_set_drvdata(cldev, ipts);
-	mei_cldev_register_rx_cb(cldev, ipts_receiver_callback);
 
-	return ipts_control_start(ipts);
+	ret = ipts_control_start(ipts);
+	if (ret) {
+		dev_err(&cldev->dev, "Failed to start IPTS: %d\n", ret);
+		return ret;
+	}
+
+	return 0;
 }
 
 static void ipts_mei_remove(struct mei_cl_device *cldev)
 {
-	int i;
+	int ret;
 	struct ipts_context *ipts = mei_cldev_get_drvdata(cldev);
 
-	ipts_control_stop(ipts);
-
-	for (i = 0; i < 20; i++) {
-		if (ipts->status == IPTS_HOST_STATUS_STOPPED)
-			break;
-
-		msleep(25);
-	}
+	ret = ipts_control_stop(ipts);
+	if (ret)
+		dev_err(&cldev->dev, "Failed to stop IPTS: %d\n", ret);
 
 	mei_cldev_disable(cldev);
-	kfree(ipts);
 }
 
 static struct mei_cl_device_id ipts_mei_device_id_table[] = {
-	{ "", IPTS_MEI_UUID, MEI_CL_VERSION_ANY },
+	{ .uuid = IPTS_MEI_UUID, .version = MEI_CL_VERSION_ANY },
 	{},
 };
 MODULE_DEVICE_TABLE(mei, ipts_mei_device_id_table);
@@ -93,33 +98,8 @@ static struct mei_cl_driver ipts_mei_driver = {
 	.probe = ipts_mei_probe,
 	.remove = ipts_mei_remove,
 };
-
-static int __init ipts_mei_init(void)
-{
-	int ret;
-
-	ret = ipts_uapi_init();
-	if (ret)
-		return ret;
-
-	ret = mei_cldev_driver_register(&ipts_mei_driver);
-	if (ret) {
-		ipts_uapi_free();
-		return ret;
-	}
-
-	return 0;
-}
-
-static void __exit ipts_mei_exit(void)
-{
-	mei_cldev_driver_unregister(&ipts_mei_driver);
-	ipts_uapi_free();
-}
+module_mei_cl_driver(ipts_mei_driver);
 
 MODULE_DESCRIPTION("IPTS touchscreen driver");
 MODULE_AUTHOR("Dorian Stoll <dorian.stoll@tmsp.io>");
 MODULE_LICENSE("GPL");
-
-module_init(ipts_mei_init);
-module_exit(ipts_mei_exit);
