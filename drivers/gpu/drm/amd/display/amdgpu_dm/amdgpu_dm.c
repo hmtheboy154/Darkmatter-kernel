4e21bfc4b05a760855824e5d437588a12219b5e0
Swapnil Patel <Swapnil.Patel@amd.com>
2023-09-26 16:24:25 -0400

    drm/amd/display: Don't add common modes for eDP connector
    [Why]
    Currently, we are adding various common modes to drm_connector for DP
    and eDP connection even if they aren't part of EDID. This results in
    unsupported modes getting added to eDP connector.
    
    [How]
    Add common modes to drm_connector only for DP connection.
    
    Signed-off-by: Swapnil Patel <swapnil.patel@amd.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -7307,7 +7307,8 @@ static int amdgpu_dm_connector_get_modes(struct drm_connector *connector)
 				drm_add_modes_noedid(connector, 1920, 1080);
 	} else {
 		amdgpu_dm_connector_ddc_get_modes(connector, edid);
-		amdgpu_dm_connector_add_common_modes(encoder, connector);
+		if (connector->connector_type != DRM_MODE_CONNECTOR_eDP)
+			amdgpu_dm_connector_add_common_modes(encoder, connector);
 		amdgpu_dm_connector_add_freesync_modes(connector, edid);
 	}
 	amdgpu_dm_fbc_init(connector);
